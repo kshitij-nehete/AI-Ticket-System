17ddb3935e4a74e8a925ee9c36522d147f5bcdbf
kshitij-nehete <kshitijnehete@gmail.com>
2025-06-03 22:33:18 +0530

    check auth jsx

--- ai-ticket-frontend/src/components/check-auth.jsx
+++ ai-ticket-frontend/src/components/check-auth.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function CheckAuth() {
-  return (
-    <div>check-auth</div>
-  )
+function CheckAuth({ children, protectedRoute }) {
+  const navigate = useNavigate();
+  const { loading, setLoading } = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (protectedRoute) {
+      if (!token) {
+        navigate.apply("/login");
+      } else {
+        setLoading(false);
+      }
+    } else {
+      if (token) {
+        navigate("/");
+      } else {
+        setLoading(false);
+      }
+    }
+  }, [navigate, protectedRoute]);
+
+  if (loading) {
+    return <div>loading...</div>;
+  } else {
+    return children;
+  }
 }
 
-export default CheckAuth
+export default CheckAuth;
