6c38a705bdb007d5c00753a24aabc313604e8f58
kshitij-nehete <kshitijnehete@gmail.com>
2025-06-03 19:29:33 +0530

    ticket.js controllers

--- ai-ticket-assistant/routes/ticket.js
+++ ai-ticket-assistant/routes/ticket.js
@@ -1,5 +1,11 @@
-import express from "express"
+import express from "express";
+import { authenticate } from "../middlewares/auth.js";
+import { createTicket, getTicket, getTickets } from "../controllers/ticket.js";
 
-const router = express.router()
+const router = express.router();
 
-export default router
+router.get("/", authenticate, getTickets);
+router.get("/:id", authenticate, getTicket);
+router.get("/", authenticate, createTicket);
+
+export default router;
